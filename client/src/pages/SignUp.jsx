419297ca27ec260de62ec9924331bf0a7bbbac69
MohdAyanKhan <ayankhanofficial35@gmail.com>
2024-08-13 23:26:49 +0530

    SignUP changes

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Link,useNavigate } from 'react-router-dom'
 import OAuth from '../components/OAuth';
+import style from "./SignUp.module.css";
 
 
 
@@ -49,39 +50,40 @@ export default function SignUp() {
 
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col gap-4 ">
+      <form
+        onSubmit={handleSubmit}
+        className={`${style.SignUpForm} flex flex-col gap-4 `}
+      >
+        <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
         <input
           type="text"
           placeholder="username"
-          className="border p-3 rounded-lg"
+          className={` ${style.SnUpinput} border p-3 rounded-lg`}
           id="username"
           onChange={handleChange}
         />
         <input
-          type="text"
+          type="email"
           placeholder="email"
-          className="border p-3 rounded-lg"
+          className={` ${style.SnUpinput} border p-3 rounded-lg`}
           id="email"
           onChange={handleChange}
         />
         <input
-          type="text"
+          type="password"
           placeholder="password"
-          className="border p-3 rounded-lg"
+          className={` ${style.SnUpinput} border p-3 rounded-lg`}
           id="password"
           onChange={handleChange}
         />
 
         <button
           disabled={loading}
-          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          className={`${style.SnUpBtn}`}
         >
           {loading ? "loading..." : "Sign Up"}
         </button>
-        <OAuth>
-          
-        </OAuth>
+        <OAuth></OAuth>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
@@ -89,7 +91,7 @@ export default function SignUp() {
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
-      {error && <p className='text-red-500 mt-t'>{error}</p>}
+      {error && <p className="text-red-500 mt-t">{error}</p>}
     </div>
   );
 
